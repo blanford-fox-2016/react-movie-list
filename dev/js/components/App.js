c6ba196a9ba17bea3199dc138accc4ece34a4715
Adhitia Hidayat <adhitia.hidayat@gmail.com>
2016-11-24 05:44:34 +0700

    re init

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
 require('../../scss/style.scss');
 
 const App = () => (
     <div>
-        <h2>User List</h2>
-        <UserList />
+        <h2>Movie List</h2>
         <hr />
-        <h2>User Details</h2>
-        <UserDetails />
     </div>
 );
 
--- dev/js/index.js
+++ dev/js/index.js
@@ -6,18 +6,12 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
 
+import App from './components/App';
 const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
+
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+        <App />,
     document.getElementById('root')
 );
